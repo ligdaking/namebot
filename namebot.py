04e5dd8811df904e1650219108502d274a63f83a
Your Nameligdaking <you@example.comevg.kostrov22@gmail.com>
2022-07-03 19:13:33 +0300

    some message about your changes

--- namebot.py
+++ namebot.py
@@ -29,6 +29,7 @@ def handle_text(message):
         bot.send_message(message.chat.id, 'Пидарок — Игорь Бабиков.')
     elif message.text == "Я пидарок":
         bot.send_message(message.chat.id, 'Я знаю.')
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text == "Стас":
