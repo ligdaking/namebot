85da6972a635fd88789348910bdf13af9f8c4075
Your Nameligdaking <you@example.comevg.kostrov22@gmail.com>
2022-07-03 18:51:02 +0300

    some message about your changes

--- namebot.py
+++ namebot.py
@@ -26,8 +26,8 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text == "Марк, кто пидарок?":
-        bot.send_message(message.chat.id1, 'Пидарок — Игорь Бабиков')
-    elif message.text == "Эль гатоо":
-        bot.send_message(message.chat.id1, 'Эль гатоо')
+        bot.send_message(message.chat.id, 'Пидарок — Игорь Бабиков.')
+    elif message.text == "Я пидарок":
+        bot.send_message(message.chat.id, 'Я знаю.')
 
 bot.polling(none_stop=True)
